2e08c85599ed167ddeddaea0c42b82568edb51fe
Zaplatynski <marza@marza.de>
2018-09-20 20:42:36 +0200

    Update to Java-Bom

--- console/src/test/java/de/marza/firstspirit/modules/logging/console/utilities/LevelTest.java
+++ console/src/test/java/de/marza/firstspirit/modules/logging/console/utilities/LevelTest.java
@@ -3,7 +3,8 @@ package de.marza.firstspirit.modules.logging.console.utilities;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertThat;
+
 import static org.hamcrest.Matchers.is;
 
 /**
@@ -11,88 +12,88 @@ import static org.hamcrest.Matchers.is;
  */
 public class LevelTest {
 
-  private Level tesling;
+	private Level tesling;
 
-  /**
-   * Sets up.
-   *
-   * @throws Exception the exception
-   */
-  @Before
-  public void setUp() throws Exception {
-    tesling = Level.DEFAULT;
-  }
+	/**
+	 * Sets up.
+	 *
+	 * @throws Exception the exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		tesling = Level.DEFAULT;
+	}
 
-  /**
-   * Refine by next fragement error.
-   *
-   * @throws Exception the exception
-   */
-  @Test
-  public void refineByNextFragementError() throws Exception {
-    final Level result = tesling.valueOfFragment("DEBUG INFO Error WARN This is a message...");
+	/**
+	 * Refine by next fragement error.
+	 *
+	 * @throws Exception the exception
+	 */
+	@Test
+	public void refineByNextFragementError() throws Exception {
+		final Level result = tesling.valueOfFragment("DEBUG INFO Error WARN This is a message...");
 
-    assertThat(result, is(Level.ERROR));
-  }
+		assertThat(result, is(Level.ERROR));
+	}
 
-  /**
-   * Refine by next fragement warning.
-   *
-   * @throws Exception the exception
-   */
-  @Test
-  public void refineByNextFragementWarning() throws Exception {
-    final Level result = tesling.valueOfFragment("DEBUG info WARN This is a message...");
+	/**
+	 * Refine by next fragement warning.
+	 *
+	 * @throws Exception the exception
+	 */
+	@Test
+	public void refineByNextFragementWarning() throws Exception {
+		final Level result = tesling.valueOfFragment("DEBUG info WARN This is a message...");
 
-    assertThat(result, is(Level.WARN));
-  }
+		assertThat(result, is(Level.WARN));
+	}
 
-  /**
-   * Refine by next fragement info.
-   *
-   * @throws Exception the exception
-   */
-  @Test
-  public void refineByNextFragementInfo() throws Exception {
-    final Level result = tesling.valueOfFragment("iNFO DEBUG This is a message...");
+	/**
+	 * Refine by next fragement info.
+	 *
+	 * @throws Exception the exception
+	 */
+	@Test
+	public void refineByNextFragementInfo() throws Exception {
+		final Level result = tesling.valueOfFragment("iNFO DEBUG This is a message...");
 
-    assertThat(result, is(Level.INFO));
-  }
+		assertThat(result, is(Level.INFO));
+	}
 
-  /**
-   * Refine by next fragement debug.
-   *
-   * @throws Exception the exception
-   */
-  @Test
-  public void refineByNextFragementDebug() throws Exception {
-    final Level result = tesling.valueOfFragment("DEBUG This is a message...");
+	/**
+	 * Refine by next fragement debug.
+	 *
+	 * @throws Exception the exception
+	 */
+	@Test
+	public void refineByNextFragementDebug() throws Exception {
+		final Level result = tesling.valueOfFragment("DEBUG This is a message...");
 
-    assertThat(result, is(Level.DEBUG));
-  }
+		assertThat(result, is(Level.DEBUG));
+	}
 
-  /**
-   * Refine by next fragement.
-   *
-   * @throws Exception the exception
-   */
-  @Test
-  public void refineByNextFragement() throws Exception {
-    final Level result = tesling.valueOfFragment("This is a message...");
+	/**
+	 * Refine by next fragement.
+	 *
+	 * @throws Exception the exception
+	 */
+	@Test
+	public void refineByNextFragement() throws Exception {
+		final Level result = tesling.valueOfFragment("This is a message...");
 
-    assertThat(result, is(Level.DEFAULT));
-  }
+		assertThat(result, is(Level.DEFAULT));
+	}
 
-  /**
-   * Refine by next fragement null.
-   *
-   * @throws Exception the exception
-   */
-  @Test
-  public void refineByNextFragementNull() throws Exception {
-    final Level result = Level.WARN.valueOfFragment(null);
+	/**
+	 * Refine by next fragement null.
+	 *
+	 * @throws Exception the exception
+	 */
+	@Test
+	public void refineByNextFragementNull() throws Exception {
+		final Level result = Level.WARN.valueOfFragment(null);
 
-    assertThat(result, is(Level.WARN));
-  }
+		assertThat(result, is(Level.WARN));
+	}
 
 }
--- console/src/test/java/de/marza/firstspirit/modules/logging/console/utilities/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Created by marza on 16.05.16.
- */
-package de.marza.firstspirit.modules.logging.console.utilities;
